fef044607370271367f228d45c31f40a24347f2c
mlekhi <maya.lekhi1@gmail.com>
2024-09-23 17:51:33 -0400

    fixing wording

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
+import React from "react";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 function App() {
   const stars = [
-    { left: '10%', top: '30%', rotation: 15 },
-    { left: '60%', top: '10%', rotation: 0 },
-    { left: '0%', top: '85%', rotation: 20 },
-    { left: '97%', top: '25%', rotation: 180 },
-    { left: '30%', top: '53%', rotation: -30 },
-    { left: '90%', top: '70%', rotation: 45 },
-    { left: '50%', top: '80%', rotation: 90 },
-    { left: '60%', top: '40%', rotation: -15 },
+    { left: "10%", top: "30%", rotation: 15 },
+    { left: "60%", top: "10%", rotation: 0 },
+    { left: "0%", top: "85%", rotation: 20 },
+    { left: "97%", top: "25%", rotation: 180 },
+    { left: "30%", top: "53%", rotation: -30 },
+    { left: "90%", top: "70%", rotation: 45 },
+    { left: "50%", top: "80%", rotation: 90 },
+    { left: "60%", top: "38%", rotation: -15 },
   ];
 
   return (
     <div className="App">
-      <div style={{ background: 'linear-gradient(to right, #273B4D, #091322)' }}>
+      <div
+        style={{ background: "linear-gradient(to right, #273B4D, #091322)" }}
+      >
         <Menu />
-        <div className="relative flex justify-center items-center h-screen">
+        <div className="relative flex flex-col justify-center items-center h-screen space-y-4">
           <h1 className="text-3xl font-bold text-white z-10">momentum</h1>
+          <p className="text-white z-10">Sundays at noon</p>
           {stars.map((star, index) => (
             <img
               key={index}
@@ -30,21 +33,45 @@ function App() {
                 left: star.left,
                 top: star.top,
                 transform: `rotate(${star.rotation}deg)`,
-                pointerEvents: 'none', // Prevent pointer events for stars
+                pointerEvents: "none", // Prevent pointer events for stars
               }}
             />
           ))}
         </div>
       </div>
       <div className="justify-center items-center p-20">
+      <div className="relative flex justify-center items-center h-screen">
         <p>
-          Momentum is a co-working community bringing together Western University’s builders,
-          creatives, technologists, designers, researchers, budding founders, and more to work on
-          their passion projects. Our goal is to foster a space that for people to connect over
-          their interests, work together, and remain accountable & dedicated to work they care
-          about.
+          We host weekly co-working sessions for you to work on your passion
+          projects with likeminded people.
         </p>
-        <h2 className="text-3xl font-bold">no schoolwork allowed</h2>
+        <h2 className="text-3xl font-bold text-black text-opacity-50">
+          Explore your curiosity.
+        </h2>
+      </div>
+        <div className="relative flex justify-center items-center h-screen">
+          <h2 className="text-3xl font-bold text-black text-opacity-50">
+            No schoolwork allowed.
+          </h2>
+          <p>
+            Momentum is a co-working community of builders, creatives,
+            technologists, designers, researchers, founders, and more to dive
+            into their passion projects through sessions in London.
+          </p>
+          <h2 className="text-3xl font-bold text-black text-opacity-50">
+            Intros + two work blocks + casual open demos
+          </h2>
+        </div>
+        <div className="relative flex justify-center items-center h-screen">
+        <h2 className="text-3xl font-bold text-black text-opacity-50">
+          You belong here.
+        </h2>
+        <div>
+          <h2>That project you keep putting off... </h2>
+          <h2>the idea that feels just out of reach... </h2>
+          <h2>big or small, now’s the time.</h2>
+        </div>
+        </div>
       </div>
       <Footer />
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-gray-800 text-white flex flex-col items-center">
+    <footer className="p-4 flex flex-col items-center">
       <p className="text-sm mb-2">
-        Made with 🤍 by Maya
+        Made with 💙 by Maya
       </p>
       <p className="text-sm">
         © {new Date().getFullYear()} Momentum. All rights reserved.
