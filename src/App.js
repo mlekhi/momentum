c864d19ed73f41197ab36976373588503d549439
mlekhi <maya.lekhi1@gmail.com>
2024-09-24 10:51:17 -0400

    fix star

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
           </div>
           <img
             src="shootingStar.webp"
-            className="absolute left-30 top-20 w-40 h-28"
+            className="absolute right-10 top-0 w-40 h-28"
           />
         </div>
         <div
