f08c0cdf6a6d27ceb4deebb5d36318b5a8822685
mlekhi <maya.lekhi1@gmail.com>
2025-02-28 11:07:21 -0500

    optimizing image loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import CommunitySection from "./components/HowSection";
@@ -31,9 +32,12 @@ const App: React.FC = () => {
       <ScrollableBanner />
       <CommunitySection />
       <div style={{ backgroundColor: "#FFF8E1" }}>
-        <img
+        <Image
           src={isMobile ? "/banner2.png" : "/banner.png"}
           alt="Banner"
+          width={1920}
+          height={600}
+          priority
         />
       </div>
       <CalltoActionSection />
